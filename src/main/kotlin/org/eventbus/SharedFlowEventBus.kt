4610f56ff45c5e775627f3b80d86828a9da4b572
ivanbadia <ivanbadia@gmail.com>
2021-03-11 10:18:22 +0100

    publish events to other subscribers when one subscriber fails

--- src/main/kotlin/org/eventbus/SharedFlowEventBus.kt
+++ src/main/kotlin/org/eventbus/SharedFlowEventBus.kt
@@ -1,27 +1,38 @@
 package org.eventbus
 
-import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.*
 import kotlinx.coroutines.Dispatchers.Default
 import kotlinx.coroutines.flow.*
-import kotlinx.coroutines.launch
+import mu.KotlinLogging
 
 class SharedFlowEventBus : EventBus {
     private val _events = MutableSharedFlow<DomainEvent>()
-    val events : SharedFlow<DomainEvent> = _events.asSharedFlow()
+    private val logger = KotlinLogging.logger {}
+    val events: SharedFlow<DomainEvent> = _events.asSharedFlow()
+    val scope = CoroutineScope(Default + CoroutineName("EventBus") + SupervisorJob())
 
     override fun publish(event: DomainEvent) {
-        CoroutineScope(Default).launch {
+        scope.launch {
             _events.emit(event)
         }
     }
 
     inline fun <reified EventType : DomainEvent> subscribe(subscriber: DomainEventSubscriber<EventType>): SharedFlowEventBus {
-        CoroutineScope(Default).launch {
+        scope.launch {
             events
                 .filter { event -> event is EventType }
-                .collect { event  -> subscriber.consume(event as EventType) }
+                .map { it as EventType }
+                .collect(sendEventTo(subscriber))
         }
         return this
     }
 
+    fun <EventType : DomainEvent> sendEventTo(subscriber: DomainEventSubscriber<EventType>): suspend (value: EventType) -> Unit =
+        { event ->
+            try {
+                subscriber.consume(event)
+            } catch (exception: Exception) {
+                logger.error(exception) { "Exception consuming event $event" }
+            }
+        }
 }
--- src/test/kotlin/org/eventbus/SharedFlowEventBusShould.kt
+++ src/test/kotlin/org/eventbus/SharedFlowEventBusShould.kt
@@ -1,37 +1,75 @@
 package org.eventbus
 
 import org.assertj.core.api.Assertions.assertThat
+import org.awaitility.kotlin.await
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import java.time.OffsetDateTime
+import java.util.concurrent.TimeUnit.SECONDS
 
 
 class SharedFlowEventBusShould {
-    private val eventBus = SharedFlowEventBus()
     private val orderCreatedSubscriber = OrderCreatedSubscriber()
     private val orderCancelledSubscriber = OrderCancelledSubscriber()
+    private val errorSubscriber = SubscriberThatFails()
+    private lateinit var eventBus : EventBus
+
+    @BeforeEach
+    internal fun setUp() {
+        eventBus = SharedFlowEventBus()
+            .subscribe(orderCreatedSubscriber)
+            .subscribe(orderCancelledSubscriber)
+            .subscribe(errorSubscriber)
+    }
 
     @Test
     fun `publish events to subscribers`() {
-        eventBus
-            .subscribe(orderCreatedSubscriber)
-            .subscribe(orderCancelledSubscriber)
         val orderCreated = OrderCreated()
         val orderCancelled = OrderCancelled()
 
         eventBus.publish(orderCreated)
         eventBus.publish(orderCancelled)
 
-        assertThat(orderCreatedSubscriber.receivedEvents)
-            .containsExactly(orderCreated)
-        assertThat(orderCancelledSubscriber.receivedEvents)
-            .containsExactly(orderCancelled)
+        await
+            .atMost(2, SECONDS)
+            .untilAsserted {
+                assertThat(orderCreatedSubscriber.receivedEvents)
+                    .containsExactly(orderCreated)
+                assertThat(orderCancelledSubscriber.receivedEvents)
+                    .containsExactly(orderCancelled)
+            }
     }
+
+
+    @Test
+    fun `publish events to other subscribers when one subscriber fails`() {
+        eventBus.publish(EventForSubscriberThatFails())
+        eventBus.publish(OrderCreated())
+        eventBus.publish(EventForSubscriberThatFails())
+        eventBus.publish(OrderCreated())
+
+        await
+            .atMost(2, SECONDS)
+            .untilAsserted {
+                assertThat(orderCreatedSubscriber.receivedEvents)
+                    .hasSize(2)
+            }
+    }
+
 }
 
 data class OrderCreated(override val occurredOn: OffsetDateTime = OffsetDateTime.now()) : DomainEvent
 
-data class OrderCancelled(override  val occurredOn: OffsetDateTime = OffsetDateTime.now()) : DomainEvent
+data class OrderCancelled(override val occurredOn: OffsetDateTime = OffsetDateTime.now()) : DomainEvent
+
+data class EventForSubscriberThatFails(override val occurredOn: OffsetDateTime = OffsetDateTime.now()) : DomainEvent
 
 class OrderCreatedSubscriber : SubscriberForTesting<OrderCreated>()
 
 class OrderCancelledSubscriber : SubscriberForTesting<OrderCancelled>()
+
+class SubscriberThatFails : DomainEventSubscriber<EventForSubscriberThatFails> {
+    override fun consume(event: EventForSubscriberThatFails) {
+        throw RuntimeException()
+    }
+}
